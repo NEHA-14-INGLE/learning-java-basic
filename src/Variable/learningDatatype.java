9e75efa425c9ba650c7f49a3e811ce0f40837d8a
Neha ingle <ingleneha152@gmail.com>
2025-08-01 22:25:18 +0530

    1 august variable

---
+++ src/Variable/learningDatatype.java
@@ -0,0 +1,9 @@
+package Variable;
+
+public class learningDatatype {
+    public static void main(String[] args) {
+        int a = 22;
+        int b = 11;
+        System.out.println(a+b);
+    }
+}
