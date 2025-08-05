37d8db82f4d258d42fa584668b54affcf4cbff7a
Neha ingle <ingleneha152@gmail.com>
2025-08-05 12:14:02 +0530

    1 august variable

---
+++ src/Operaters_Increment_and_Decrement/operaters.java
@@ -0,0 +1,11 @@
+package Operaters_Increment_and_Decrement;
+
+public class operaters {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
--- src/Variable/learningDatatype.java
+++
@@ -1,9 +0,0 @@
-package Variable;
-
-public class learningDatatype {
-    public static void main(String[] args) {
-        int a = 22;
-        int b = 11;
-        System.out.println(a+b);
-    }
-}
